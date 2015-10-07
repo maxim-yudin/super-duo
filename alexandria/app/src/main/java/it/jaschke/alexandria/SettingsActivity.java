f32e1bf2eca75b9761c3518f0217c1bd20da6e65
Maxim Yudin <maxim.yudin@gmail.com>
2015-10-07 07:41:23 +0300

    Using preference fragment instead deprecated PreferenceActivity

--- alexandria/app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
@@ -1,18 +1,24 @@
 package it.jaschke.alexandria;
 
+
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by saj on 27/01/15.
- */
-public class SettingsActivity extends PreferenceActivity {
-
+public class SettingsActivity extends AppCompatActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-
-
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+    
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
